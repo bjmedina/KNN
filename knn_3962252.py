bd3a296c200027c1e241845aa54d3630ab182dfb
Bryan Medina <bjmedina.cs@gmail.com>
2019-09-15 17:41:36 -0400

    Organized data using Pandas. TODO: Command line arguments for required for the hw. Need one for the k value (k-nearest neighbors) and another for the distance metric.

---
+++ knn_3962252.py
@@ -0,0 +1,15 @@
+# Bryan Medina
+# CAP5610 - Machine Learning
+# K Nearest Neighbors w/ Iris Dataset
+
+####### Imports ########
+import numpy as np
+import pandas as pd
+########################
+
+# TODO #
+# Get command line inputs #
+# Need to get k and distance metric
+
+####### Getting data ###
+data = pd.read_csv('/Users/bjm/Documents/School/fall2019/CAP5610/assignments/a1/data/iris.data') # Change data directory
